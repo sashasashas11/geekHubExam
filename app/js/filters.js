3bc73a665b0f68e41c1572b9a3670707e780b31a
Sasha <dimurchik@ukr.net>
2014-04-19 19:04:09 +0300

    add filter by brands

--- app/js/filters.js
+++ app/js/filters.js
@@ -4,10 +4,17 @@ geekHubFilter.filter('goodsFilter', function () {
     return function (items, filter) {
         var filtered = [];
 
-        // My filter
+        if (filter.brand.length != 0 && filter.brand.length != undefined) {
+            angular.forEach(filter.brand, function (brand) {
+                var productBrand = brand;
+                angular.forEach(items, function (item) {
+                    if (item.brand == productBrand)
+                        filtered.push(item);
+                })
+            });
+        }
+        else return items;
 
         return filtered;
-
     }
-
 })
